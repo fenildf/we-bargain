9767ef3f60bf1742ae42d7a5ee06d5cfbb8e7720
MarcoPai <mr.pai@msn.com>
2015-06-08 20:28:55 +0800

    add

---
+++ js/bargain.js
@@ -0,0 +1,11 @@
+/**
+ * bargain
+ * @authors Your Name (you@example.org)
+ * @date    2015-06-08 20:21:40
+ * @version $Id$
+ */
+
+React.render(
+        <h1>Hello, world!</h1>,
+        document.getElementById('content');
+      );
