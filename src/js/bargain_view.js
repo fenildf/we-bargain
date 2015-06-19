e288562d115728ebff934bb013916bae7d128d89
MarcoPai <mr.pai@msn.com>
2015-06-19 16:57:27 +0800

    大部分功能都完成了
    包括ajax时的弹层等待提示，点击分享后的弹层提示

--- src/js/bargain_view.js
+++ src/js/bargain_view.js
@@ -90,16 +90,14 @@ var AjaxDialog = React.createClass({
   render: function() {
     var cls = this.state.show ? 'js-modal-open' : 'js-modal-close'
     return (
-      <UI.Modal {...this.props} title={false} closeIcon={false} className={cls}>
+      <UI.Modal {...this.props} className={cls}>
         <UI.Image src="src/img/bargaining.gif" className="am-center" width="150" responsive />
       </UI.Modal>
     );
   }
 });
 
-var ajaxStart = {
-
-};
+var ajaxStart = {};
 
 
 /* ============== Course View Pages ============= */
@@ -109,54 +107,49 @@ var ajaxStart = {
  * @return {[type]}     [description]
  */
 
-var modal = (
+var loadingModal = (
   <UI.Modal title={false} closeIcon={false}>
     <UI.Image src="src/img/bargaining.gif" className="am-center" width="150" responsive />
   </UI.Modal>
 );
+var ShareModal = React.createClass({
+  render: function() {
+    return (
+      <div className="share_box">
+        <div className="am-modal am-modal-no-btn am-modal-active" tabindex="-1" id="doc-modal-1">
+          <div className="am-modal-dialog">
+            <div className="am-modal-bd">{this.props.content}</div>
+          </div>
+        </div>
+        <div className="am-dimmer am-active" data-am-dimmer=""></div>
+      </div>
+    );
+  }
+});
 
-var modalInstance = (
-  <UI.ModalTrigger
-    modal={modal}>
-    <UI.Button amStyle="primary">Loading 窗口</UI.Button>
-  </UI.ModalTrigger>
-);
-//首页倒计时
-function updateEndTime(boxs) {
-    var box = $(boxs);
-    if (box.length === 0) {
-        return false;
-    }
-    var date = new Date();
-    var time = date.getTime(); //当前时间距1970年1月1日之间的毫秒数
-    box.each(function(i) {
-        var endDate = this.getAttribute("endTime"); //结束时间字符串
-        //转换为时间日期类型
-        var endDate1 = eval('new Date(' + endDate.replace(/\d+(?=-[^-]+$)/, function(a) {
-            return parseInt(a, 10) - 1;
-        }).match(/\d+/g) + ')');
-        var endTime = endDate1.getTime(); //结束时间毫秒数
-        var lag = (endTime - time) / 1000; //当前时间和结束时间之间的秒数
-        var nMS = 2000000000 * 1000 - date.getTime();
-        if (lag > 0) {
-            var nMS2 = Math.floor(nMS / 100) % 10;
-            var second = pad(Math.floor(lag % 60));
-            var minite = pad(Math.floor((lag / 60) % 60)) ;
-            var hour = pad(Math.floor((lag / 3600) % 24)) ;
-            // xue.log(hour % 24);
-            var day = pad(Math.floor((lag / 3600) / 24));
-            $(this).html(day + "天" + hour + "时" + minite + "分" + second + "." + nMS2 + "秒");
-        } else
-            $(this).html("优惠已经结束啦！");
-    });
-   setTimeout(function() {
-        updateEndTime(box);
-    }, 100);
-   function pad(n) {
-    return (n < 10 ? '0' : '') + n;
+
+// var modalInstance = (
+//   <UI.ModalTrigger
+//     modal={modal}>
+//     <UI.Button amStyle="primary">Loading 窗口</UI.Button>
+//   </UI.ModalTrigger>
+// );
+
+
+var SetIntervalMixin = {
+  componentWillMount: function() {
+    this.intervals = [];
+  },
+  setInterval: function() {
+    this.intervals.push(setInterval.apply(null, arguments));
+  },
+  componentWillUnmount: function() {
+    this.intervals.map(clearInterval);
   }
 };
+
 var CountDown = React.createClass({
+  mixins: [SetIntervalMixin], // 引用 mixin
   getInitialState : function(){
     return {
       stat : false,
@@ -168,40 +161,81 @@ var CountDown = React.createClass({
   },
   componentDidMount : function(){
     this.handleCount();
-    setInterval(this.handleCount, 1000);
+    this.setInterval(this.handleCount, 1000); // 调用 mixin 的方法
   },
   handleCount : function(d){
-    // var starttime = new Date(this.props.start * 1000);
-    var startTime = this.props.start * 1000;
-    // var endtime = new Date(this.props.end * 1000);
+    var _state = {
+      stat   : false,
+      day    : 0,
+      hour   : 0,
+      minite : 0,
+      second : 0
+    };
+    function pad(n) {
+      return (n < 10 ? '0' : '') + n;
+    }
+    var startTime = new Date();
+    startTime = startTime.getTime();
     var endTime = this.props.end * 1000;
     var lag = (endTime - startTime) / 1000; //当前时间和结束时间之间的秒数
+
+
+    // console.log('===============');
+    // console.log('end: ' + endTime);
+    // console.log('str: ' + startTime);
+
+
+
+    // var nowTime = startTime;
+    // var svrTime = this.props.start * 1000;
+    // var newTime = {
+    //   start : svrTime - nowTime,
+    //   end : endTime
+    // };
+    // console.log('s: ' + newTime.start);
+    // console.log('e: ' + newTime.end);
+    // console.log('-----------------------');
+
+    // // var lag = (newTime.end - newTime.start) / 1000; //当前时间和结束时间之间的秒数
+    // // 
+    // // 
+    // var ln = (newTime.end - newTime.start); //当前时间和结束时间之间的秒数
+
+    // ln = ln + nowTime;
+    // console.log('ln: ' + ln);
+    // lag = ln / 1000;
+
     var content = '';
     if (lag > 0) {
-        var nMS2 = Math.floor(nMS / 100) % 10;
-        var second = pad(Math.floor(lag % 60));
-        var minite = pad(Math.floor((lag / 60) % 60)) ;
-        var hour = pad(Math.floor((lag / 3600) % 24)) ;
-        // xue.log(hour % 24);
-        var day = pad(Math.floor((lag / 3600) / 24));
-        $(this).html(day + "天" + hour + "时" + minite + "分" + second + "." + nMS2 + "秒");
+      _state.stat = true;
+      _state.hour = pad(Math.floor((lag / 3600) % 24)) ;
+      _state.minite = pad(Math.floor((lag / 60) % 60)) ;
+      _state.second = pad(Math.floor(lag % 60));
+      _state.day = pad(Math.floor((lag / 3600) / 24));
     } else{
-        $(this).html("优惠已经结束啦！");
+        _state.stat = false;
+        this.intervals.map(clearInterval);
     }
-    this.setState({
-      hour   : d.hour,
-      minite : d.minite,
-      second : d.second
-    });
+    this.setState(_state);
   },
   render: function() {
+    var _text = (
+        <p className="am-text-sm am-text-center countdown_text" round>
+          砍价已结束
+        </p>
+      );
+      if(this.state.stat){
+        _text = (
+          <p className="am-text-sm am-text-center countdown_text" round>
+            砍价倒计时：
+            <strong className="am-text-danger">{this.state.hour}</strong> 小时
+            <strong className="am-text-danger">{this.state.minite}</strong> 分
+            <strong className="am-text-danger">{this.state.second}</strong> 秒
+          </p>
+        );
+      }
     return (
-      <p className="am-text-sm am-text-center countdown_text" round>
-        砍价倒计时：
-        <strong className="am-text-danger">{this.state.hour}</strong> 小时
-        <strong className="am-text-danger">{this.state.minite}</strong> 分
-        <strong className="am-text-danger">{this.state.second}</strong> 秒
-      </p>
+      <div>{_text}</div>
     );
   }
 });
@@ -211,36 +245,51 @@ var CourseContent = React.createClass({
     return {stat:0};
   },
   componentDidMount : function(){
-    return (<AjaxDialog />);
+    // return (<AjaxDialog />);
+  },
+  handleShare : function(){
+    this.setState({ stat: 2 });
   },
   handleBargain : function(cid, uid){
-        // {modalInstance}
-
-    // $.ajax({
-    //   url : '/Welfares/firstBargain/' + cid,
-    //   dataType : 'json',
-    //   beforeSend : function(){
-    //   },
-    //   success : function(result){
-    //     var d = xue.ajaxCheck(result);
-    //     if(!d){
-    //       return false;
-    //     }
-    //     this.setState({stat: 1});
-    //   }.bind(this),
-    //   error : function(){}.bind(this)
-    // });
-  },
-  handleAssist : function(cid, uid){
+    var that = this;
     $.ajax({
-      url : '/Welfares/assistBargain/' + cid + '/' + uid,
+      url : '/Welfares/firstBargain/' + cid,
       dataType : 'json',
+      beforeSend : function(){
+        that.setState({stat: 1});
+      }.bind(this),
+      complete : function(){
+        setTimeout(function(){
+          that.setState({stat: 0});
+        }, 2000);
+      }.bind(this),
       success : function(result){
         var d = xue.ajaxCheck(result);
         if(!d){
           return false;
         }
-        this.setState({stat: 2});
+      }.bind(this),
+      error : function(){}.bind(this)
+    });
+  },
+  handleAssist : function(cid, uid){
+    var that = this;
+    $.ajax({
+      url : '/Welfares/assistBargain/' + cid + '/' + uid,
+      dataType : 'json',
+      beforeSend : function(){
+        that.setState({stat: 1});
+      }.bind(this),
+      complete : function(){
+        setTimeout(function(){
+          that.setState({stat: 0});
+        }, 2000);
+      }.bind(this),
+      success : function(result){
+        var d = xue.ajaxCheck(result);
+        if(!d){
+          return false;
+        }
       }.bind(this),
       error : function(){}.bind(this)
     });
@@ -259,10 +308,11 @@ var CourseContent = React.createClass({
     if(_data.type != 1){
       // 帮TA砍价
       btn = (
-        <div className="coursebtn_single">
-          <UI.ModalTrigger modal={AjaxDialog}>
-          <a href="#/assist" onClick={this.handleAssist.bind(this, this.props.cid, _data.stuId)} className="am-btn am-btn-warning am-btn-lg am-btn-center am-round course_btn">帮TA砍价</a>
-          </UI.ModalTrigger>
+        <div className="">
+          {/*<UI.ModalTrigger modal={modal}>*/}
+          <a href="#/assist" onClick={this.handleAssist.bind(this, this.props.cid, _data.stuId)} className="am-btn am-btn-warning am-btn-lg am-fl am-margin-left-sm am-round course_btn">帮TA砍价</a>
+          {/*</UI.ModalTrigger>*/}
+          <a href="/Welfares/" className="am-btn am-btn-primary am-btn-lg am-fr am-round course_btn am-margin-right-sm">我也要参加</a>
           {/*<UI.Button amSize="lg" amStyle="warning" round className="am-center course_btn">帮TA砍价</UI.Button>*/}
         </div>
       );
@@ -271,23 +321,22 @@ var CourseContent = React.createClass({
         // 立即购买
         btn = (
           <div className="">
-          <a href={"/ShoppingCart/addCart/"+ this.props.cid + "-0-0-0"} className="am-btn am-btn-danger am-btn-lg am-btn-center am-round am-fl am-margin-left-sm course_btn">立即购买</a>
+          <a href={"/ShoppingCart/addCart/"+ this.props.cid + "-0-0-0"} className="am-btn am-btn-danger am-btn-lg am-center am-round am-fl am-margin-left-sm course_btn">立即购买</a>
             {/*<UI.Button amSize="lg" amStyle="danger" round className="course_btn am-fl am-margin-left-sm">立即购买</UI.Button>*/}
-            <UI.Button amSize="lg" amStyle="success" round className="course_btn am-fr am-margin-right-sm">分享给朋友帮砍价</UI.Button>
+            <UI.Button amSize="lg" onClick={this.handleShare} amStyle="primary" round className="course_btn am-fr am-margin-right-sm">分享给朋友帮砍价</UI.Button>
           </div>
         );
         // 倒计时
         countdown = (
             <CountDown start={_data.bargainStartTime} end={_data.bargainEndTime} />
-            {/*<p className="am-text-sm am-text-center countdown_text" round>砍价倒计时：<strong className="am-text-danger">23</strong> 小时</p>*/}
         );
       }else{
         // 先砍一下
         btn = (
         <div className="coursebtn_single">
-          <UI.ModalTrigger modal={modal}>
+          {/*<UI.ModalTrigger modal={modal}>*/}
         	   <a href="#/bargain" onClick={this.handleBargain.bind(this, this.props.cid)} className="am-btn am-btn-warning am-btn-lg am-btn-center am-round course_btn">先砍一下</a>
-        	</UI.ModalTrigger>
+        	{/*</UI.ModalTrigger>*/}
           {/*<UI.Button amSize="lg" amStyle="warning" round className="am-center course_btn">先砍一下</UI.Button>*/}
         </div>
         );  
@@ -300,8 +349,20 @@ var CourseContent = React.createClass({
     		<div className="am-fl am-padding-left">已砍到 <strong className="am-text-danger am-text-lg">{_data.bargainPrice}</strong> 元</div>
     	);
     }
+    var dialog = '';
+    if(this.state.stat == 1){
+      dialog = (loadingModal);
+    }else if(this.state.stat == 2){
+      var user_text = (<p>
+        <span className="am-text-danger"> {this.props.username} </span>
+        正在热砍学而思网校福利课程，快来帮忙砍价，一起嗨！
+        </p>
+      );
+      dialog = (<ShareModal content={user_text} />);
+    }
     return (
       <div>
+        {dialog}
         <UI.Article title={_data.courseName} meta={_video}>
           <h2 className="am-padding-left courseview_teacher">{_data.teacherName} 老师 等你来砍价</h2>
           <UI.Article.Child role="divider" className="course_divider" />
@@ -315,6 +376,9 @@ var CourseContent = React.createClass({
             {btn}
           </UI.ButtonToolbar>
           <div className="countdown am-margin-top-sm">{countdown}</div>
+          <div className="follow_wechat am-margin-top-sm">
+            <UI.Button amStyle="success" round amSize="sm " className="am-center">关注学而思网校参加砍价</UI.Button>
+          </div>
         </UI.Article>
         <BargainUsers users={this.props.users} />
       </div>
@@ -345,7 +409,7 @@ var CourseLoadData = React.createClass({
   },
   render: function() {
     return (
-      <CourseContent data={this.state.data} users={this.state.users} cid={this.props.id} />
+      <CourseContent data={this.state.data} users={this.state.users} cid={this.props.id} username={this.props.username} />
     );
   }
 });
@@ -375,6 +439,6 @@ var BargainUsers = React.createClass({
 });
 /* ============== Router ============= */
 React.render(
-  <CourseLoadData url={bargain.courseUrl} id={bargain.courseId} />,
+  <CourseLoadData url={bargain.courseUrl} id={bargain.courseId} username={bargain.myself} />,
   mountNode
 );
